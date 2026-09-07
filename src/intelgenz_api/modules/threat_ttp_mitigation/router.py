da68afdbdf39c088e8aa8d0faaaa7b0a45529f72
threai2025 <threai2025@gmail.com>
2026-09-07 23:26:38 +0530

    chore: remove duplicate Swagger tag

--- src/intelgenz_api/modules/threat_ttp_mitigation/router.py
+++ src/intelgenz_api/modules/threat_ttp_mitigation/router.py
@@ -2,5 +2,5 @@ from fastapi import APIRouter
 
 from intelgenz_api.modules.threat_ttp_mitigation.malware import router as malware_router
 
-router = APIRouter(prefix="/threat-ttps", tags=["threat TTPs"])
+router = APIRouter(prefix="/threat-ttps")
 router.include_router(malware_router)
